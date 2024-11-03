ba1cfd71ea3acb0e5c5b70121055f84adf573056
NaduYT <kff638397@gmail.com>
2024-11-03 00:18:48 -0700

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=y3R2WQqL#CeCb0cZ44YsssUwLclIDFkrdbOup6etk23NoJCowXA4",
+MONGODB: process.env.MONGODB || "mongodb+srv://NaduYT:805640618NCCRDCC@cluster0.y30bp.mongodb.net/",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
 };
